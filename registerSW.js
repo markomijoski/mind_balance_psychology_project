7d859a6fdcef6766532af579dd5394e4f1003b09
Marko <marko.mijoski123@gmail.com>
2026-06-06 18:43:07 +0200

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mind_balance_psychology_project/sw.js', { scope: '/mind_balance_psychology_project/' })})}
--- src/components/LandingLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export default function LandingLayout() {
-  return (
-    <div className="min-h-screen bg-bg-page text-text-main font-sans transition-colors duration-300 overflow-x-hidden">
-      <Outlet />
-    </div>
-  );
-}
